c391923f79202e73f64b8cb15844fee60cbc6c94
Rasmus Lerdorf <rasmus@lerdorf.com>
2026-04-03 05:37:57 -0400

    Defensive fix - make sure we have Tls13

--- run.ps1
+++ run.ps1
@@ -10,9 +10,11 @@ param (
 $ErrorActionPreference = "Stop"
 
 # Ensure TLS 1.2/1.3 on older .NET / Windows PowerShell
-[Net.ServicePointManager]::SecurityProtocol = `
-    [Net.SecurityProtocolType]::Tls12 -bor `
-    [Net.SecurityProtocolType]::Tls13
+$securityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
+if ([Enum]::GetNames([Net.SecurityProtocolType]) -contains "Tls13") {
+    $securityProtocol = $securityProtocol -bor [Net.SecurityProtocolType]::Tls13
+}
+[Net.ServicePointManager]::SecurityProtocol = $securityProtocol
 
 $versions = @{
     "7.0" = "vc14"
